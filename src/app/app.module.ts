8bc8e2d143704cdef7ffc8610fa6336414294055
Bolaji Adeyemi <d.bola1@gmail.com>
2019-08-16 05:29:02 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ContactUsComponent,
+    PartnersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/partners/partners.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partners',
+  templateUrl: './partners.component.html',
+  styleUrls: ['./partners.component.scss']
+})
+export class PartnersComponent implements OnInit {
+
+  partners = ['Mitsubishi', 'Beijer Electronic', 'Frank Medical'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
